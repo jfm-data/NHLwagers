9bf88232b0b3b2c15c67c0ea908b94921fa7a8d8
jfm-data <57269047+jfm-data@users.noreply.github.com>
2021-03-31 15:34:50 -0300

    Add files via upload

--- streamlit_OU.py
+++ streamlit_OU.py
@@ -16,23 +16,34 @@ import statsmodels.api as sm
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
 geolocator = Nominatim(user_agent='myuseragent')
+import lxml
+import plotly.express as px
 
 from PIL import Image
 
-image = Image.open('logo.png')
 
+#with open("styles/style.css") as f:
+#    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-st.image(image,use_column_width=True)
-st.title('2021 - NHL Wager Talk')
+image = Image.open('vegas_odds.jpg')
+
+st.title('NHL Wager Analytics - 2021')
+st.image(image, use_column_width=True)
+
 
 # Load data 
-data_load_state = st.text('Loading data...')
+data_load_state = st.text('Checking and Fetching Data...')
+
+#####################################
+#### Data Gathering and Cleaning ####
+#####################################
 
 master_df = pd.read_csv('master_df.csv')
 start = pd.to_datetime(master_df.Date[-1:]).dt.date.values[0]+datetime.timedelta(days=1)
 today = datetime.date.today()
 yesterday = today-datetime.timedelta(days = 1) 
 
+#Function to covert dates to string
 def covert_dates(date1, date2):
     covert_list = []
     days = pd.date_range(date1, date2, freq='d')
@@ -40,13 +51,14 @@ def covert_dates(date1, date2):
         covert_list.append(int(days[i].strftime('%Y%m%d')))
     return covert_list
 
-
+#Function to fetch missing data
+@st.cache
 def get_data(date1, date2):
     new_df = pd.DataFrame()
     for day in covert_dates(date1, date2):
             site = f"https://sportsdatabase.com/nhl/query?output=default&sdql=date%3D{day}&submit=++S+D+Q+L+%21++"
             hdr = {'User-Agent': 'Mozilla/5.0'}
-            req = Request(site,headers=hdr)
+            req = Request(site, headers=hdr)
             page = urlopen(req)
             soup = BeautifulSoup(page)
             tables = soup.find('table', attrs={'id':'DT_Table'})
@@ -55,13 +67,16 @@ def get_data(date1, date2):
             time.sleep(1)
     return new_df
 
+#Check if the data needs updating
 if start <= yesterday:
     new_data = get_data(start, yesterday).dropna(thresh=9)
     master_df = pd.concat([master_df, new_data])
-    
+
+#Save updated data as csv    
 #master_df.to_csv("master_df.csv", index=False)
     
 raw_data = pd.read_csv('master_df.csv')
+
 
 def clean_data(df):
     df.Date =pd.to_datetime(df.Date)
@@ -70,6 +85,8 @@ def clean_data(df):
     df.insert(2, "Days_Rest", (df.Date_Prev-df.Date)*-1)
     df = df.drop(['index','Season', 'P1', 'P2', 'P3'], axis=1)
     return df
+
+#Fucntion to identify a team change to break streak counts
 
 def trips(home_or_away, TeamChange, Site):
      list =[]
@@ -86,8 +103,9 @@ def trips(home_or_away, TeamChange, Site):
          list.append(x)
      return list
 
+#Function to calculate the distance the road team is from home
+
 def distance_calc(df):
-    temp_df = df.copy()
     df.insert(4,"Team_City", df.Team.map(team_dict['City']))
     df.insert(6,"Opp_City", df.Opp.map(team_dict['City']))
     df.insert(9,"Team_point", df.Team.map(team_dict['Citypoint']))
@@ -97,7 +115,9 @@ def distance_calc(df):
     df['Opp_distance'] = df.apply(lambda x: 0 if x.Site == "away" else x.Distance, axis=1)
     df = df.drop(['Team_point','Distance','Opp_point'], axis=1)
     return df
- 
+
+#Function to count the current streak of home or games 
+
 def road_trips(df):
     df.insert(4, "TeamChange", df["Team"].shift(1, fill_value=df["Team"].head(1)) != df["Team"])
     df.insert(10, "Home_Stand", trips("home", df.TeamChange, df.Site))
@@ -109,12 +129,24 @@ def road_trips(df):
     df = df.drop('TeamChange', axis=1)
     return df
 
+#Function to pair games into a singel record -- for O/U analysis
 def opp_func (df):
     df.insert(2,"Opp_Days_Rest", eda_df.Oppkey.map(opp_days_rest))
     df.insert(10,"Opp_home_stand", eda_df.Oppkey.map(opp_home_stand))
     df.insert(11,"Opp_road_trip", eda_df.Oppkey.map(opp_road_trip))
     return df
 
+#Func to calculate the unit return of each game and team
+def unit_value(Line, Result):
+    if Line < 0 and Result == 'W':
+        return 1
+    elif Line < 0 and Result == 'L':
+        return Line/100
+    elif Line > 0 and Result == 'W':
+        return Line/100
+    elif Line > 0 and Result == 'L':
+        return -1
+    
 nhltable= pd.read_csv('nhltable.csv')
 team_dict = nhltable.set_index('Team').to_dict()
 
@@ -122,7 +154,7 @@ eda_df = clean_data(master_df)
 eda_df = distance_calc(eda_df)
 eda_df = road_trips(eda_df)
 
-#map the opponent data
+#Create keys for pairing
 Teamkey = []
 Oppkey = []
 for i in range(len(eda_df.Date)):
@@ -135,6 +167,7 @@ opp_home_stand = dict(zip(Teamkey, eda_df.Home_Stand))
 opp_road_trip = dict(zip(Teamkey, eda_df.Road_Trip))
 opp_func(eda_df)
 
+
 eda_df = pd.concat([eda_df, pd.get_dummies(eda_df.OUr)], axis=1)
 
 goals_df = eda_df['Final'].str.split('-', expand=True).rename(columns={0:'Team_Goals', 1:'Opp_Goals'}).astype(int)
@@ -142,10 +175,11 @@ goals_df = eda_df['Final'].str.split('-', expand=True).rename(columns={0:'Team_G
 eda_df = pd.concat([eda_df, goals_df], axis=1)
 eda_df['total_O'] = eda_df.groupby('Team')['O'].cumsum()
 eda_df['total_U'] = eda_df.groupby('Team')['U'].cumsum()
+eda_df['total_P'] = eda_df.groupby('Team')['P'].cumsum()
 eda_df['total_Team_goals'] = eda_df.groupby('Team')['Team_Goals'].cumsum()
 eda_df['total_Opp_goals'] = eda_df.groupby('Team')['Opp_Goals'].cumsum()
-eda_df = eda_df.loc[eda_df['OUr']!='P']
-eda_df['y'] = (eda_df.OUr=='O').astype(int)
+#eda_df = eda_df.loc[eda_df['OUr']!='P']
+#eda_df['y'] = (eda_df.OUr=='O').astype(int)
 
 eda_df['Team_U'] = eda_df.groupby('Team')['total_U'].transform('max')
 eda_df['Team_O'] = eda_df.groupby('Team')['total_O'].transform('max')
@@ -160,26 +194,64 @@ eda_df['Opp_Goals_Allowed'] = eda_df.groupby('Opp')['total_Opp_goals'].transform
 
 #Seperating the two EDA dataframes
 eda_OU = eda_df.loc[eda_df['Site']=='home']
+eda_OU.insert(3, "Combined_Rest", eda_OU.loc[:,'Days_Rest'] + eda_OU.loc[:,'Opp_Days_Rest']) 
+cut_labels = [500, 1000, 1500, 2000, 3000, 4000]
+cut_bins = [0, 500, 1000, 1500, 2000, 3000, 4000]
+eda_OU['Distance'] = pd.cut(eda_OU.loc[:,'Opp_distance'], bins=cut_bins, labels= cut_labels)
 eda_OU = eda_OU.sort_values('Date').reset_index(drop=True)
 
 # Notify user that the data was successfully loaded.
-data_load_state.text('Loading data...done!')
+data_load_state.text('Done & done!')
 
+
+
+
+#############################################
+###  Streamlit Design  ######################
+############################################
 
 if st.button('Hide Raw Data'):
     st.write('Data Table Hidden')
 else:
-    st.subheader('Raw data')
+    st.subheader("Tonight's Games --(Work in progress)")
     st.dataframe(eda_df.style.highlight_max(axis=0))
 
 
-st.subheader('O/U Analysis')
-st.subheader('Select Parameters')
+st.header('O/U Analysis')
+
+st.subheader('Overall')
+fig_OU = px.histogram(eda_OU, x="Total", color='OUr',
+                    barmode='group', template='plotly_dark')
+st.plotly_chart(fig_OU, use_container_width=True)
+
+st.subheader('By Combined Days Rest')
+fig2 = px.histogram(eda_OU[eda_OU["Combined_Rest"] <10], x="Combined_Rest", color='OUr',
+                   barmode='group', template='plotly_dark')
+st.plotly_chart(fig2, use_container_width=True)
+
+st.subheader('By Distance of Road Team from Home')
+fig3 = px.histogram(eda_OU, x="Distance", color='OUr',
+                   barmode='group', template='plotly_dark')
+st.plotly_chart(fig3, use_container_width=True)
+
+st.subheader('By Length of Road Trip')
+fig4 = px.histogram(eda_OU, x="Opp_road_trip", color='OUr',
+                   barmode='group', template='plotly_dark')
+st.plotly_chart(fig4, use_container_width=True)
+
+st.subheader('By Length of Home Stand')
+fig5 = px.histogram(eda_OU, x="Home_Stand", color='OUr',
+                   barmode='group', template='plotly_dark')
+st.plotly_chart(fig5, use_container_width=True)
+
+
+
+#st.subheader('Select Parameters for situational outputs')
 
 #Filtering For Days of Rest
-Days_to_filter = st.slider('Days of Rest', 0, max(eda_OU.Days_Rest), 3)
-st.text('Number of Days Rest %s' % Days_to_filter)
-filtered_data = eda_OU[eda_OU['Days_Rest'] == Days_to_filter]
+#Days_to_filter = st.slider('Days of Rest', 0, max(eda_OU.Days_Rest), 3)
+#st.text('Number of Days Rest %s' % Days_to_filter)
+#filtered_data = eda_OU[eda_OU['Days_Rest'] == Days_to_filter]
 
 #Filtering For Distance
 #Distance_to_filter = st.slider('Distance of Opponent', 0.0, max(data.Distance), (0.0, 500.0))
@@ -190,12 +262,26 @@ filtered_data = eda_OU[eda_OU['Days_Rest'] == Days_to_filter]
 
 
 #Filtering For Home and Away
-st.subheader('Unit Analysis')
+st.header('O/U Team Analysis')
+team_select = st.selectbox("Select Team",
+                 list(pd.unique(eda_df.Team)))
+st.write('You selected', team_select)
+
+filtered_data = eda_df[eda_df['Team'] == team_select]
+
 home_away = st.selectbox("Is the Team Home or Away?",
-                 ('Home', 'Away'))
+                 ('home', 'away'))
 st.write('You selected', home_away)
 
+filtered_data = filtered_data[filtered_data['Site'] == home_away]
 
+days_rest = st.slider('Days Rest', 0, 5, 2)
+filtered_data = filtered_data[filtered_data['Days_Rest'] == days_rest]
+
+st.subheader('O/U by Selected Inputs')
+fig_OU_team = px.histogram(filtered_data, x="Total", color='OUr',
+                    barmode='group', template='plotly_dark')
+st.plotly_chart(fig_OU_team, use_container_width=True)
 
 #Filtering For Distance
 #Distance_to_filter = st.slider('Distance From Home', 0.0, max(data.Distance), (0.0, 500.0))
@@ -213,7 +299,7 @@ st.write('You selected', home_away)
 #else:
 #    st.write("You didn't select comedy.")
 
-import plotly.express as px
+
 
 #fig = px.histogram(data, x="Date_diff", color='OUr',
 #                   barmode='group', template='plotly_white')
@@ -230,16 +316,15 @@ import plotly.express as px
 #                    barmode='group', template='plotly_white')
 #st.plotly_chart(fig2, use_container_width=True)
 
-
+st.header('Unit Analysis')
+unit_team = st.selectbox("Select Team for Unit",
+                 list(pd.unique(eda_df.Team)))
+st.write('You selected', unit_team)
 
 
 #Filter for OU Line
-Line_to_filter = st.slider('O/U Line', 0.0, max(eda_OU.Total), (0.0, 5.5))
-filtered_data2 = filtered_data[(eda_OU['Total'] >= Line_to_filter[0]) &
-                              (eda_OU['Total'] <= Line_to_filter[1])]
+#Line_to_filter = st.slider('Unit Line', 0.0, max(eda_OU.Total), (0.0, 5.5))
+#filtered_data2 = filtered_data[(eda_OU['Total'] >= Line_to_filter[0]) &
+#                              (eda_OU['Total'] <= Line_to_filter[1])]
 
 
-st.subheader('O/U Totals')
-fig3 = px.histogram(eda_OU, x="Total", color='OUr',
-                    barmode='group', template='plotly_white')
-st.plotly_chart(fig3, use_container_width=True)
